0eead7c5bd425085b3c665d7a9eddbcc612d39b2
Maysummer <udojyk@gmail.com>
2022-04-19 13:23:05 -0700

    test cases

--- test/3-main.c
+++ test/3-main.c
@@ -3,18 +3,20 @@
 /**
  * main - Entry point
  *
+ * Test for different possible outcomes
+ *
  * Return: Always 0
  */
 int main(void)
 {
     _printf("%b\n", 98);
     _printf("%b\n", 0);
-    _printf("%o\n", 98);
+    /*_printf("%o\n", 98);
     _printf("%x\n", 56741);
     _printf("%X\n", -56741);
     _printf("%u\n", 0);
     _printf("i %i\n", -INT_MAX);
     _printf("d %d\n", -INT_MAX);
-    _printf("%d\n", 0);
+    _printf("%d\n", 0);*/
     return (0);
 }
