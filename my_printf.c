711225cc444c873da393e4b504198f6bfad21832
EbubeCode <chukwuma258@gmail.com>
2022-04-20 11:45:32 +0100

    solve task 0

--- my_printf.c
+++ my_printf.c
@@ -11,45 +11,33 @@
  */
 int check_flags(char *a, va_list lis, char *buff)
 {
-	int i = 0, x;
+	int i = 0, x = 0;
 
-	while (*a != '\0')
+	if (*a == 'c')
+		buff[i++] = (char)va_arg(lis, int);
+	else if (*a == 'b')
+		x = print_b(va_arg(lis, unsigned int), &buff[i]);
+	else if (*a == 'x' || *a == 'X')
+		x = print_x(va_arg(lis, unsigned int), *a == 'x', &buff[i]);
+	else if (*a == 'o')
+		x = print_o(va_arg(lis, unsigned int), &buff[i]);
+	else if (*a == 'u')
+		x = print_u(va_arg(lis, unsigned int), &buff[i]);
+	else if (*a == 's')
+		x = print_s(va_arg(lis, char *), &buff[i]);
+	else if (*a == 'S')
+		x = print_S(va_arg(lis, char *), &buff[i]);
+	else if (*a == 'd' || *a == 'i')
+		x = print_d(va_arg(lis, int), &buff[i]);
+	else if (*a == '%')
+		buff[i++] = '%';
+	else
 	{
-		x = 0;
-		if (*a == '%')
-		{
-			a++;
-			if (*a == 'c')
-				buff[i++] = (char)va_arg(lis, int);
-			else if (*a == 'b')
-				x = print_b(va_arg(lis, unsigned int), &buff[i]);
-			else if (*a == 'x' || *a == 'X')
-				x = print_x(va_arg(lis, unsigned int), *a == 'x', &buff[i]);
-			else if (*a == 'o')
-				x = print_o(va_arg(lis, unsigned int), &buff[i]);
-			else if (*a == 'u')
-				x = print_u(va_arg(lis, unsigned int), &buff[i]);
-			else if (*a == 's')
-				x = print_s(va_arg(lis, char *), &buff[i]);
-			else if (*a == 'S')
-				x = print_S(va_arg(lis, char *), &buff[i]);
-			else if (*a == 'd' || *a == 'i')
-				x = print_d(va_arg(lis, int), &buff[i]);
-			else if (*a == '%')
-				buff[i++] = '%';
-			else
-			{
-				buff[i++] = '%';
-				buff[i++] = *a;
-			}
-		}
-		else
-			buff[i++] = *a;
-		if (x == -1)
-			return (x);
-		i += x;
-		a++;
+		buff[i++] = '%';
+		buff[i++] = *a;
 	}
+
+	i += x;
 	return (i);
 }
 /**
@@ -60,16 +48,33 @@ int check_flags(char *a, va_list lis, char *buff)
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
+	int i = 0, x;
 	va_list lis;
-	char *a = (char *)format, *buff = malloc(sizeof(char) * 1024);
+	char *a = (char *)format, *buff = malloc(sizeof(char) * 2000);
 
 	if (format == NULL || buff == NULL)
 		return (-1);
 	va_start(lis, format);
-	i = check_flags(a, lis, buff);
+	while(*a != '\0')
+	{
+		x = 0;
+		if (*a == '%')
+		{
+			a++;
+			if (*a != '\0')
+				x = check_flags(a, lis, buff);
+			else
+				break;
+			i += x;
+		}
+		else
+			buff[i++] = *a;
+		a++;
+	}
 	if (i > 0)
 		write(1, buff, i);
+	else
+		i = -1;
 	va_end(lis);
 	free(buff);
 	return (i);
